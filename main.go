2e024b6c1fcf7bc93fd7716a6fc9a4f83694b754
ponchik327 <seva-enkin@mail.ru>
2025-01-03 19:39:03 +0300

    Update get and post methods
    get: displays all records from the database;\npost: now loads tasks into the database

--- main.go
+++ main.go
@@ -8,32 +8,46 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var task string
-
-type RequestBody struct {
-	Task string `json:"task"`
-}
-
 func GetHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s", task)
+	var msgs []Message
+	result := DB.Find(&msgs)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusBadRequest)
+	}
+
+	for i, msg := range msgs {
+		fmt.Fprintf(w, "задача номер: %d, Task=%s, Is_done=%v\n", i, msg.Task, msg.IsDone)
+	}
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	var reqBody RequestBody
-	err := json.NewDecoder(r.Body).Decode(&reqBody)
+	var msg Message
+	err := json.NewDecoder(r.Body).Decode(&msg)
 	if err != nil {
 		http.Error(w, "Ошибка при декодировании JSON", http.StatusBadRequest)
 		return
 	}
 
-	task = reqBody.Task
-	fmt.Fprintf(w, "В переменную task записано значение: %s\n", task)
+	result := DB.Create(&msg)
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusBadRequest)
+	}
+
+	fmt.Fprintf(w, "В базу данных добавлена задача: %s, со статусом %v\n", msg.Task, msg.IsDone)
 }
 
 func main() {
+	// Вызываем метод InitDB() из файла db.go
+	InitDB()
+
+	// Автоматическая миграция модели Message
+	DB.AutoMigrate(&Message{})
+
 	router := mux.NewRouter()
+
 	// наше приложение будет слушать запросы на localhost:8080/api/hello
 	router.HandleFunc("/api/tasks", GetHandler).Methods("GET")
 	router.HandleFunc("/api/tasks", PostHandler).Methods("POST")
+
 	http.ListenAndServe(":8080", router)
 }
